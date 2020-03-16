d78f9c34503fc9c89ab7f29ddfd2aaab4b80c879
grregd <grregd@gmail.com>
2020-03-16 20:10:40 +0100

    parsing functions extracted to separate module

--- ESP-01/http_server_leds/http_server_leds.ino
+++ ESP-01/http_server_leds/http_server_leds.ino
@@ -4,15 +4,14 @@
   Complete project details at https://randomnerdtutorials.com
 *********/
 
-
 #include "handlers.h"
+#include "parsing_config.h"
 
 // Load Wi-Fi library
 #include <ESP8266WiFi.h>
 #include <WiFiClientSecure.h>
 #include <string>
 #include <vector>
-#include <ArduinoJson.h>
 
 
 using namespace Handlers;
@@ -155,94 +154,5 @@ void loop()
   }
 }
 
-String fetchConfig(const String & id)
-{
-
-//  "https://raw.githubusercontent.com/grregd/smarthome/master/README.md?token=AAG2525BSVQHFDAQ5WDI6JK6IXB6M";
-  String url = "/grregd/smarthome/master/configs/config_" + id;
-  static const char host[] = "raw.githubusercontent.com";
-  const int httpsPort = 443;
-
-  static const char fingerprint[] = "CC:AA:48:48:66:46:0E:91:53:2C:9C:7C:23:2A:B1:74:4D:29:9D:33";
-
-  Serial.print("fetchConfig from ");
-  Serial.println(String(host) + url);
-
-  WiFiClientSecure client;
-
-  client.setFingerprint(fingerprint);
-
-  if (!client.connect(host, httpsPort)) {
-    Serial.println("connection failed");
-    return {};
-  }
-
-  if (client.verify(fingerprint, host)) {
-    Serial.println("certificate matches");
-  } else {
-    Serial.println("certificate doesn't match");
-  }
 
 
-  client.print(String("GET ") + url + " HTTP/1.1\r\n" +
-               "Host: " + host + "\r\n" +
-               "User-Agent: BuildFailureDetectorESP8266\r\n" +
-               "Connection: close\r\n\r\n");
-
-  while (client.connected()) {
-    String line = client.readStringUntil('\n');
-    if (line == "\r") {
-      Serial.println("headers received");
-      Serial.println(line);
-      break;
-    }
-  }
-
-  String result;
-  while (client.available()) {
-    String line = client.readStringUntil('\n');
-    result += line;
-    Serial.println(line);
-    //    if (line.startsWith("{\"state\":\"success\"")) {
-    //      Serial.println("esp8266/Arduino CI successfull!");
-    //    } else {
-    //      Serial.println("esp8266/Arduino CI has failed");
-    //    }
-  }
-
-  Serial.println("fetchConfig success");
-
-  return result;
-}
-
-void parseConfig( const String & configText,
-                  std::function<void(int port,
-                                     int activeLevel,
-                                     const String& headerMarker,
-                                     const String& num,
-                                     bool logic)> callback)
-{
-  StaticJsonDocument<1024> configDocument;
-
-  auto error = deserializeJson(configDocument, configText);
-  if (error)
-  {
-    Serial.print(F("deserializeJson() failed: "));
-    Serial.println(error.c_str());
-    return;
-  }
-
-  auto config = configDocument["config"];
-  Serial.println(config["ModuleName"].as<String>());
-  auto outputs = config["outputs"].as<JsonArray>();
-  for (const JsonObject & output : outputs)
-  {
-    Serial.println(".");
-    callback(
-      output["port"].as<int>(),
-      output["initial"].as<String>() == "LOW" ? LOW : HIGH,
-      output["headerMarker"].as<String>(),
-      output["num"].as<String>(),
-      true );
-  }
-}
---
+++ ESP-01/http_server_leds/parsing_config.h
@@ -0,0 +1,13 @@
+#include <Arduino.h>
+#include <functional>
+
+String fetchConfig(const String & id);
+
+void parseConfig( const String & configText,
+                  std::function<void(int port,
+                                     int activeLevel,
+                                     const String& headerMarker,
+                                     const String& num,
+                                     bool logic)> callback);
+
+
