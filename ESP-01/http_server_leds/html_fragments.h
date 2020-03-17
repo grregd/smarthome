761204eea2d69606900038e8a721d85388101a21
grregd <grregd@gmail.com>
2020-03-17 23:44:56 +0100

    html tempaltes extracted to separate file

---
+++ ESP-01/http_server_leds/html_fragments.h
@@ -0,0 +1,19 @@
+
+static const std::string http200(
+  "HTTP/1.1 200 OK"
+  "Content-type:text/html"
+  "Connection: close");
+
+static const std::string htmlHead(
+  "<!DOCTYPE html><html>"
+  "<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">"
+  "<link rel=\"icon\" href=\"data:,\">"
+  "<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}"
+  ".button { background-color: #195B6A; border: none; color: white; padding: 16px 40px;"
+  "text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}"
+  ".button2 {background-color: #77878A;}</style></head>");
+  
+static const std::string htmlBodyBeg("<body><h1>ESP8266 Web Server</h1>");
+
+static const std::string htmlBodyEnd("</body></html>");
+
--- ESP-01/http_server_leds/http_server_leds.ino
+++ ESP-01/http_server_leds/http_server_leds.ino
@@ -14,14 +14,13 @@
 #include <string>
 #include <vector>
 
-
 using namespace Handlers;
 
 std::vector<Handler> handlers;
 
 // Replace with your network credentials
-const char* ssid     = "";
-const char* password = "";
+const char *ssid = "";
+const char *password = "";
 
 // Set web server port number to 80
 WiFiServer server(80);
@@ -42,7 +41,8 @@ void setupPort(int port)
   digitalWrite(port, LOW);
 }
 
-void setup() {
+void setup()
+{
   Serial.begin(115200);
   Serial.println();
   Serial.println(__LINE__);
@@ -54,11 +54,11 @@ void setup() {
   // Connect to Wi-Fi network with SSID and password
   Serial.println(ssid);
   WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
+  while (WiFi.status() != WL_CONNECTED)
+  {
     delay(500);
     Serial.print(".");
   }
-
 
   // Print local IP address and start web server
   Serial.println("");
@@ -66,19 +66,16 @@ void setup() {
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
 
-  String ipStr{ WiFi.localIP().toString() };
+  String ipStr{WiFi.localIP().toString()};
 
   parseConfig(
-    fetchConfig(ipStr.substring(ipStr.lastIndexOf('.') + 1)),
-    [](int port, int activeLevel,
-       const String & headerMarker,
-       const String & num, bool logic)
-      {
-        handlers.push_back(Handler(port, activeLevel, 
-            headerMarker.c_str(), num.c_str(), logic));
-      }
-  );
-
+      fetchConfig(ipStr.substring(ipStr.lastIndexOf('.') + 1)),
+      [](int port, int activeLevel,
+         const String &headerMarker,
+         const String &num, bool logic) {
+        handlers.push_back(Handler(port, activeLevel,
+                                   headerMarker.c_str(), num.c_str(), logic));
+      });
 
   initAll(handlers);
 
@@ -87,33 +84,37 @@ void setup() {
 
 void loop()
 {
-  WiFiClient client = server.available();   // Listen for incoming clients
+  WiFiClient client = server.available(); // Listen for incoming clients
 
   delay(100);
 
-  if (client) {                             // If a new client connects,
+  if (client)
+  { // If a new client connects,
     Serial.println(__LINE__);
-    Serial.println("New Client.");          // print a message out in the serial port
-    String currentLine = "";                // make a String to hold incoming data from the client
+    Serial.println("New Client."); // print a message out in the serial port
+    String currentLine = "";       // make a String to hold incoming data from the client
     currentTime = millis();
     previousTime = currentTime;
-    while (client.connected() && currentTime - previousTime <= timeoutTime) { // loop while the client's connected
+    while (client.connected() && currentTime - previousTime <= timeoutTime)
+    { // loop while the client's connected
       currentTime = millis();
-      if (client.available()) {             // if there's bytes to read from the client,
-        char c = client.read();             // read a byte, then
+      if (client.available())
+      {                         // if there's bytes to read from the client,
+        char c = client.read(); // read a byte, then
         header += c;
-        if (c == '\n') {                    // if the byte is a newline character
+        if (c == '\n')
+        { // if the byte is a newline character
           // if the current line is blank, you got two newline characters in a row.
           // that's the end of the client HTTP request, so send a response:
-          if (currentLine.length() == 0) {
+          if (currentLine.length() == 0)
+          {
             // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
             // and a content-type so the client knows what's coming, then a blank line:
-            client.println(http200);
+            client.println(http200.c_str());
             client.println();
 
             handleAllInput(handlers, header);
 
-              
             client.println(htmlHead.c_str());
             client.println(htmlBodyBeg.c_str());
             handleAllOuput(client, handlers);
@@ -123,11 +124,15 @@ void loop()
             client.println();
             // Break out of the while loop
             break;
-          } else { // if you got a newline, then clear currentLine
+          }
+          else
+          { // if you got a newline, then clear currentLine
             currentLine = "";
           }
-        } else if (c != '\r') {  // if you got anything else but a carriage return character,
-          currentLine += c;      // add it to the end of the currentLine
+        }
+        else if (c != '\r')
+        {                   // if you got anything else but a carriage return character,
+          currentLine += c; // add it to the end of the currentLine
         }
       }
     }
@@ -139,6 +144,3 @@ void loop()
     Serial.println("");
   }
 }
-
-
-
