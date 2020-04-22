89c0cf382d753819526660527d4be7bdb8fa400d
grregd <grregd@gmail.com>
2020-04-23 01:02:58 +0200

    smaller buttons

--- lib/html_fragments.h
+++ lib/html_fragments.h
@@ -7,8 +7,8 @@ static const char htmlHead[] = "<!DOCTYPE html><html> \
     <head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\"> \
     <link rel=\"icon\" href=\"data:,\"> \
     <style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;} \
-    .button { background-color: #195B6A; border: none; color: white; padding: 16px 40px; \
-    text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;} \
+    .button { background-color: #195B6A; border: none; color: white; padding: 4px 30px; \
+    text-decoration: none; font-size: 12px; margin: 2px; cursor: pointer;} \
     .button2 {background-color: #77878A;}</style></head>";
 
 static const char htmlBodyBeg[] = "<body><h1>ESP8266 Web Server</h1>";
